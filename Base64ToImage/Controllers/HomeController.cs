0879bb23427e73a65541fccfbdfbc3c4d4d6cdc0
ParthKansara <pkansara@uniworldtechnologies.com>
2020-12-07 19:44:51 +0530

    initial commit

---
+++ Base64ToImage/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Base64ToImage.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Base64ToImage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        /// <summary>
+        /// Render Demo page
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            ImageViewModel imageViewModel = new ImageViewModel();
+            return View(imageViewModel);
+        }
+        /// <summary>
+        /// View Image
+        /// </summary>
+        /// <param name="imageViewModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Index(ImageViewModel imageViewModel)
+        {
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            Base64ToImage(imageViewModel.Base64String).Save(webRootPath + "\\Images\\sample.png", System.Drawing.Imaging.ImageFormat.Png);
+            imageViewModel.ImageUrl = "~/Images/sample.png";
+            return View(imageViewModel);
+        }
+
+        /// <summary>
+        /// Convert base64 string to Image
+        /// </summary>
+        /// <returns></returns>
+        public System.Drawing.Image Base64ToImage(string base64String)
+        {
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            MemoryStream ms = new MemoryStream(imageBytes, 0, imageBytes.Length);
+            ms.Write(imageBytes, 0, imageBytes.Length);
+            System.Drawing.Image image = System.Drawing.Image.FromStream(ms, true);
+            return image;
+        }
+
+    }
+}
---
+++ Base64ToImage/Models/ImageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Base64ToImage.Models
+{
+    public class ImageViewModel
+    {
+        public string Base64String { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
